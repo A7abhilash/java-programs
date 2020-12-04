2fdb6200f620c55de212767272413d8b49f56095
A7abhilash <a7abhilash@gmail.com>
2020-12-04 07:00:05 +0530

    changed folder names

--- LabCycle-2/Program1.java
+++
@@ -1,5 +0,0 @@
-//WAP to generate and print prime numbers for the given range;
-import java.util.Scanner;
-    public class Program1{
-        
-}
--- LabCycle-2/Program2.java
+++
@@ -1,5 +0,0 @@
-//WAP to find the roots of a quadratic equation;
-import java.util.Scanner;
-public class Program2{
-  
-}
